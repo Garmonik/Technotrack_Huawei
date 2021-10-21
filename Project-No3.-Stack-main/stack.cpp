ec394d4da9b4441dac206bb11f7d35095876c386
MarkKolyadich <70436486+Garmonik@users.noreply.github.com>
2021-10-21 15:30:02 +0300

    Add files via upload

---
+++ Project-No3.-Stack-main/stack.cpp
@@ -0,0 +1,151 @@
+#include "defense.h"
+
+void clear_file(const char* file_log_name){
+    FILE* file = fopen(file_log_name, "w");
+    fclose(file);
+}
+
+Stack_t* stack_construct(Stack_t* stack, long capacity){   
+    stack_null(stack);
+    clear_file("log_stack.txt");
+
+    if (capacity == 0){
+        stack->capacity            = (size_t) capacity;
+        stack->size                = 0;
+        stack->data                = nullptr;
+        stack->left_struct_canary  = Canary;
+        stack->right_struct_canary = Canary;
+
+        stack_realloc(stack);
+    }
+
+    else if(capacity > 0){
+        stack->capacity            = (size_t) capacity;
+        stack->size                = 0;
+        stack->data                = nullptr;
+        stack->left_struct_canary  = Canary;
+        stack->right_struct_canary = Canary;
+        
+        #ifdef PROTECTION 
+            void* memory = calloc(1, (stack->capacity) * sizeof(elem_t) + 2 * sizeof(canary_t));
+            
+            if (memory == nullptr){
+                stack->error = MEMORY_OUT;
+                assert_ok(stack);
+            }
+
+            placing_canaries(stack, memory);
+            poison_fill(stack, stack->size, stack->capacity);
+
+            stack->hash = stack_hash(stack);
+        #else 
+            stack->data = (elem_t*) calloc(1, (stack->capacity) * sizeof(elem_t));
+        #endif
+
+        assert_ok (stack);
+    }
+
+    else if (capacity < 0){
+        stack->error = NEGATIVE_CAPACITY;
+        assert_ok (stack);
+    }
+
+    return stack;
+}
+
+void stack_push(Stack_t* stack, elem_t value){
+    assert_ok(stack);
+    stack_null(stack);
+
+    if((stack->size + 1) > (stack->capacity)){
+        stack_realloc (stack);
+    }
+
+    *(stack->data + stack->size) = value;
+    stack->size++;
+
+    stack->hash = stack_hash(stack);
+
+    assert_ok(stack);
+}
+
+elem_t stack_pop(Stack_t* stack){
+    assert_ok(stack);
+    stack_null(stack);
+
+    elem_t out = *(stack->data + stack->size - 1);
+
+    poison_fill(stack, stack->size - 1, stack->size);
+
+    stack -> size--;
+    stack -> hash = stack_hash(stack);
+    
+    assert_ok(stack);
+
+    return out;
+}
+
+void stack_destruct(Stack_t* stack){
+    assert_ok(stack);
+    
+    #ifdef PROTECTION
+        free((canary_t*) (stack -> data) - 1);
+    #else
+        free(stack->data);
+    #endif
+
+    stack->data                = nullptr;       
+    stack->size                = DATA_POISON; 
+    stack->error               = 0;           
+    stack->capacity            = DATA_POISON;             
+    stack->left_struct_canary  = Canary - 1;  
+    stack->right_struct_canary = Canary - 1;
+}
+
+void stack_realloc(Stack_t* stack){
+    stack_null(stack);
+
+    if(stack->capacity == 0){
+        stack->capacity = INITIAL_CAPACITY;
+
+        #ifdef PROTECTION 
+            void* memory = calloc(1, stack->capacity * sizeof (elem_t) + 2 * sizeof(canary_t));
+
+            if(memory == nullptr){
+                stack->error = MEMORY_OUT;
+                assert_ok (stack);
+            }
+
+            placing_canaries(stack, memory);
+            poison_fill(stack, stack->size, stack->capacity);
+
+            stack->hash = stack_hash(stack);
+        #else
+            stack->data = (elem_t*)calloc(1, stack->capacity * sizeof(elem_t));
+        #endif 
+
+        assert_ok(stack);
+    }
+
+    else if(stack->capacity == stack->size){
+        size_t old_capacity = stack->capacity;
+        
+        #ifdef PROTECTION 
+            void* memory = realloc((canary_t*)(stack->data) - 1, (stack->capacity *= X_CAPACITY) * sizeof (elem_t) + 2 * sizeof(canary_t));
+
+            if(memory == nullptr){
+                stack->error = MEMORY_OUT;
+                assert_ok(stack);
+            }
+
+            placing_canaries(stack, memory);
+            poison_fill(stack, old_capacity, stack -> capacity);
+
+            stack->hash = stack_hash (stack);
+        #else 
+            stack->data = (elem_t*)realloc(stack->data, (stack->capacity *= X_CAPACITY) * sizeof(elem_t));
+        #endif
+        
+        assert_ok(stack);
+    }
+}
